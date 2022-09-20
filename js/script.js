92b030e7207bf62dc1929718b1850703dbc47e02
kairat <jumanov7070@gmail.com>
2022-09-20 08:01:52 +0600

    js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let navbar = document.querySelector('.main_navbar');
+let ul = document.querySelector('main_nav_menu');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    show();
+}
+document.querySelector('.closemenu').onclick = () => {
+    close();
+}
+
+
+function show ()   {
+    document.querySelector('.main_navbar').style.display = 'flex';
+    document.querySelector('.main_nav_menu').style.display = "flex";
+
+    document.querySelector('.video_play').style.display = 'none';
+    document.body.style.overflow = 'hidden';
+    
+    
+}
+
+function close(){
+    document.querySelector('.main_navbar').style.display = 'none';
+    document.querySelector('.main_nav_menu').style.display = "none";
+
+}
+
+
+window.onscroll = function() {
+    let distanceScrolled = document.documentElement.scrollTop;
+    
+    if (distanceScrolled == 0) {
+      document.querySelector('.navbar').style.backgroundColor = 'transperant'
+    }
+    else {
+        document.querySelector('.navbar').style.backgroundColor = '#ED4C5C';
+
+    }
+}
---
+++ Новая папка/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.main_navbar');
+let ul = document.querySelector('main_nav_menu');
+document.querySelector('#menu-btn').onclick = () =>{
+    show();
+}
+
+function show ()   {
+    document.body.style.color = 'black';
+}
